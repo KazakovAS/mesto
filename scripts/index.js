77ce93b67a93a84a0dbb5bf3d66913b233fd04e8
Kazakov Alexey <a.kazakov@betaonline.ru>
2022-01-25 00:31:28 +0300

    готовая верстка и js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 const userNickname = document.querySelector('.user__nickname-text');
 const userDescription = document.querySelector('.user__description');
 const placesEl = document.querySelector('.places__list');
+const userInfoEditButton = document.querySelector('.user__info-edit-button');
+const popup = document.querySelector('.popup');
+const popupOverlay = document.querySelector('.popup__overlay');
+const popupCloseButton = document.querySelector('.popup__close-button');
 
 let places = [
     {
@@ -19,7 +23,7 @@ let places = [
         imageAlt: 'Фото Домбая'
     },
     {
-        name: 'Карачаевск',
+        name: 'Карачаево-Черкессия',
         imagePath: './assets/images/karachaevsk.jpg',
         imageAlt: 'Фото Карачаевска'
     },
@@ -51,7 +55,11 @@ function fillPlaces() {
                     >
                     <div class="place__info">
                         <h2 class="place__name">${place.name}</h2>
-                        <button class="place__like-button"></button>
+                        <button
+                            class="place__like-button"
+                            aria-label="Нравится"
+                        >
+                        </button>
                     </div>
                 </article>
             </li>
@@ -59,8 +67,48 @@ function fillPlaces() {
     }
 };
 
+function popupOpened() {
+    popup.classList.add('popup_opened');
+    setUserInfoEditFormFieldValue();
+};
+
+function popupClosed() {
+    popup.classList.remove('popup_opened');
+};
 
 setTextValue(userNickname,'Жак-Ив Кусто');
 setTextValue(userDescription, 'Исследователь океана');
 
 fillPlaces();
+
+userInfoEditButton.addEventListener('click', popupOpened);
+
+popupOverlay.addEventListener('click', function(e) {
+    if(e.target === popupOverlay) popup.classList.remove('popup_opened');
+});
+
+popupCloseButton.addEventListener('click', popupClosed);
+
+placesEl.addEventListener('click', function(e) {
+    if(e.target.classList.contains('place__like-button')) e.target.classList.toggle('place__like-button_active');
+});
+
+
+const userNicknameField = document.getElementById('user-nickname-field');
+const userDescriptionField = document.getElementById('user-description-field');
+
+function setUserInfoEditFormFieldValue() {
+    userNicknameField.value = userNickname.textContent;
+    userDescriptionField.value = userDescription.textContent;
+}
+
+const userInfoEditForm = document.getElementById('user-info-edit-form');
+
+userInfoEditForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    setTextValue(userNickname, userNicknameField.value);
+    setTextValue(userDescription, userDescriptionField.value);
+
+    popupClosed();
+});
