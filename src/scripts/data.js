804b42451dd51aef602a0e9d55d4c8438992faad
Kazakov Alexey <a.kazakov@betaonline.ru>
2022-04-09 18:37:50 +0300

    Homework-8: add webpack start

---
+++ src/scripts/data.js
@@ -0,0 +1,32 @@
+const karachaevskImage = new URL('../assets/images/karachaevsk.jpg', import.meta.url);
+const elbrusImage = new URL('../assets/images/elbrus.jpg', import.meta.url);
+const dombayImage = new URL('../assets/images/dombay.jpg', import.meta.url)
+
+const places = [
+  {
+    name: 'Карачаевск',
+    image: karachaevskImage
+  },
+  {
+    name: 'Гора Эльбрус',
+    image: elbrusImage
+  },
+  {
+    name: 'Домбай',
+    image: dombayImage
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    image: karachaevskImage
+  },
+  {
+    name: 'Гора Эльбрус',
+    image: elbrusImage
+  },
+  {
+    name: 'Домбай',
+    image: dombayImage
+  }
+];
+
+export default places;
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './build'),
+    port: 8080,
+    open: true,
+    client: {
+      reconnect: true,
+      progress: true,
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(webp|png|svg|jpe?g|gif|ico)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff?2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                config: path.resolve(__dirname, "postcss.config.js"),
+              }
+            }
+          }
+        ]
+      },
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                quality: 95,
+              },
+              webp: {
+                lossless: 1,
+              },
+              avif: {
+                cqLevel: 0,
+              },
+            },
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html'
+    }),
+
+    // Automatic creation any html pages (Don't forget to RERUN dev server)
+    // ...baseWebpackConfig.externals.paths_pages.map(
+    //   (page) =>
+    //     new HtmlWebpackPlugin({
+    //       template: `${baseWebpackConfig.externals.paths_dir}/${page}`,
+    //       filename: `./${page}`,
+    //     }),
+    // ),
+
+    new CleanWebpackPlugin()
+  ]
+}
