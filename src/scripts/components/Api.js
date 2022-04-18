54bd22059efff262f445457bfd381bc6044f9765
Kazakov Alexey <a.kazakov@betaonline.ru>
2022-04-18 05:03:27 +0300

    промежуточный

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -71,8 +71,8 @@ class Api {
 export const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-37',
   headers: {
-    authorization: '2e5af95a-25b4-4742-9ead-1326c8073602',
-    // authorization: '9e805e1a-4ae4-4e34-b6d0-ba7f164bd419',
+    // authorization: '2e5af95a-25b4-4742-9ead-1326c8073602',
+    authorization: '9e805e1a-4ae4-4e34-b6d0-ba7f164bd419',
     'Content-Type': 'application/json'
   }
 });
